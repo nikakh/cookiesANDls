ef597d02a0c3d02f117e53681e753ada8e1cc214
Nikakh <n_khonelidze2@cu.edu.ge>
2018-09-07 23:14:43 +0400

    123

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp',['ngCookies','ngStorage']);
-app.controller('todoList', function($scope, $cookies, $localStorage){
+app.controller('todoList', function($scope, $cookies, $localStorage, $window){
 	var myCookie = $cookies.get('task');
 	var parse = JSON.parse($cookies.get('task') || 'null');
 	var lsParse = JSON.parse(localStorage.getItem('task') || 'null');
@@ -29,12 +29,49 @@ app.controller('todoList', function($scope, $cookies, $localStorage){
 	    
 	  }
 
-	  $scope.addToDoLS = function(val){
+	 $scope.addToDoLS = function(val){
 	  	$scope.todos.push({text:$scope.newToDo, done:false});
 	  	localStorage.setItem('task',JSON.stringify($scope.todos));
 	  	$scope.newToDo = '';
 	  }
 
+	  $scope.contentEdit = function(msg){ 
+		 
+		  for(i=0; i<$scope.todos.length; i++){
+			  if($scope.todos[i].text == msg){
+				  $scope.todos[i].text = event.target.innerText;
+			  }
+		  }	;	  
+		  $cookies.put('task', JSON.stringify($scope.todos));
+		  
+		event.target.contentEditable = event.target.contentEditable == "false" ? "true" : "false";
+		console.log($scope.todos);
+	  };
+
+
+	  $scope.enterAgain = function(msg) {
+		  if(event.which == 13 && msg != ""){
+			  $scope.contentEdit(msg);
+			  console.log(11);
+		  }
+	  }
+	  $scope.changed = function(){
+		$cookies.put('task', JSON.stringify($scope.todos));
+		localStorage.setItem('task',JSON.stringify($scope.todos));
+
+	  }
+
+		
+	  $scope.deleteSelected = function(j){
+
+		$scope.todos.splice(j,1);
+		localStorage.removeItem(j);
+		
+		$cookies.put('task', JSON.stringify($scope.todos));
+		localStorage.setItem('task',JSON.stringify($scope.todos))
+	  }	  
+		  
+	  
 });
 
 
